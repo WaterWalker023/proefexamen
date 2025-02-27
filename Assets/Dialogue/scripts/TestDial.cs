b88e5efac5c1e37a1202ba48823cf1dd993ed4f6
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2025-02-27 15:31:32 +0100

    string isent being parsed to the ai

--- Assets/Dialogue/scripts/TestDial.cs
+++ Assets/Dialogue/scripts/TestDial.cs
@@ -21,31 +21,41 @@ public class TestDial : MonoBehaviour
     private bool _once;
     public async void AskChatGPT(string newText)
     {
-        
         ChatMessage devMessage = new ChatMessage
         {
-            Content = "Your James the local bar owner. you like being left alone but are a good listener. You only respond to what is being asked. you know the towns people better than any one. " +
-                      "Those towns people are: Sweets the farmer,Baron the flecther,Kronie,Fauna the forest witch, Mumei the Knight",
+            Content = "you are:"+
+                      "NPC: " +
+                      "James; " +
+                      "NPC job: " +
+                      "Town Bar owner; " +
+                      "NPC Description: " +
+                      "James is an older fellow that runs the only bar on this island and thus has interacted with a lot of people." +
+                      "He has a lot of drinks like chocomelk, koffie and thee. " +
+                      "He also has a deer head he shot himself he is realy proud of that. " +
+                      "He also was on a cool adventure that he cant stop talking about" +
+                      "Other NPC residents: " +
+                      "Sweets the farmer,Baron the flecther,Kronie the Mage,Fauna the forest witch, Mumei the Knight; ",
             Role = "developer"
         };
         if (!_once)
         {
-            _messages.Add(devMessage);
+            //_messages.Add(devMessage);
             _once = true;
         }
     
         ChatMessage newMessage = new ChatMessage
         {
             Content = newText,
-            Role = "system"
+            Role = "user"
         };
-
+        Debug.Log(newText);
         _messages.Add(newMessage);
 
         CreateChatCompletionRequest request = new CreateChatCompletionRequest
         {
             Messages = _messages,
-            Model = "gpt-3.5-turbo"
+            Model = "gpt-3.5-turbo",
+            Temperature = 0.85f
         };
 
         var response = await _openAI.CreateChatCompletion(request);
