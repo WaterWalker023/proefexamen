3899179b5a41e3b2896d2920e27987b2c9cbb233
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2025-02-27 09:42:54 +0100

    added OpenAi package and basic dialogue with openAi
    the bot does not do exactly what we need it to. needs a better promt to start with

---
+++ Assets/Dialogue/scripts/TestDial.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenAI;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+
+public class TestDial : MonoBehaviour
+{
+    public OnResponseEvent onResponse;
+    
+    [Serializable]
+    public class OnResponseEvent: UnityEvent<string>
+    {
+        
+    }
+    private OpenAIApi _openAI = new (Environment.GetEnvironmentVariable("OPENAI_API_KEY", EnvironmentVariableTarget.User));
+    private List<ChatMessage> _messages = new List<ChatMessage>();
+    private bool _once;
+    public async void AskChatGPT(string newText)
+    {
+        
+        ChatMessage devMessage = new ChatMessage
+        {
+            Content = "Your James the local bar owner. you like being left alone but are a good listener. you know the towns people better than any one. " +
+                      "Those towns people are: Sweets the farmer,Baron the flecther,Kronie,Fauna the forest witch, Mumei the Knight",
+            Role = "developer"
+        };
+        if (!_once)
+        {
+            _messages.Add(devMessage);
+        }
+    
+        ChatMessage newMessage = new ChatMessage
+        {
+            Content = newText,
+            Role = "system"
+        };
+
+        _messages.Add(newMessage);
+
+        CreateChatCompletionRequest request = new CreateChatCompletionRequest
+        {
+            Messages = _messages,
+            Model = "gpt-3.5-turbo"
+        };
+
+        var response = await _openAI.CreateChatCompletion(request);
+       
+        if (response.Choices != null && response.Choices.Count > 0)
+        {
+            var chatResponse = response.Choices[0].Message;
+            _messages.Add(chatResponse);
+            
+            Debug.Log(chatResponse.Content);
+            
+            onResponse.Invoke(chatResponse.Content);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
