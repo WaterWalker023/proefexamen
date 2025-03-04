9e16d3f6c9325e69c6fb6ed6025e848e9752838a
WaterWalker023 <90682548+WaterWalker023@users.noreply.github.com>
2025-03-04 15:18:58 +0100

    start on the first minegame

--- Assets/minigames/binary/binaryminigame.cs
+++ Assets/minigames/binary/binaryminigame.cs
@@ -1,14 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class binaryminigame : MonoBehaviour
 {
     private List<int> binarynumbers = new List<int>();
     private int total;
+    private int getto;
+    
+    public UnityEvent onWin;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        getto = Random.Range(0, 32);
     }
 
     // Update is called once per frame
@@ -19,9 +23,10 @@ public class binaryminigame : MonoBehaviour
         {
             total += binarynumbers[i];
         }
-        if (total == 15)
+        if (total == getto)
         {
-            Debug.Log("done");
+            onWin.Invoke();
+            getto = Random.Range(0, 32);
         }
         Debug.Log(total);
     }
--- Assets/player/Overlapbox.cs
+++ Assets/player/Overlapbox.cs
@@ -13,7 +13,7 @@ public class Overlapbox : MonoBehaviour
     void FixedUpdate()
     {
         MyCollisions();
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             Interact();
         }
